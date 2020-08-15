a52c99c3e37401c6a4f87eb56aaf2fd4d31c8fb2
AMAN123956 <56161073+AMAN123956@users.noreply.github.com>
2020-08-15 10:02:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 160 || document.documentElement.scrollTop > 160) {
+    document.getElementById("navbar").style.top = "-50px";
+  } else {
+    document.getElementById("navbar").style.top = "0px";
+  }
+}
